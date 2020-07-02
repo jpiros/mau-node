d6a5b7fc5535d5cea553c23794c0db54ab3b90dd
Jason Piros <jasonpiros@gmail.com>
2020-07-01 20:34:54 -0700

    route a request

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const http = require('http');
 
 const server = http.createServer(( req, res ) => {
-  console.log(req.url, req.method, req.headers);
-  
+  const url = req.url;
+
+  if (url === '/') {
+    res.write(`
+      <html>
+        <head>
+          <title>IMS</title>
+        </head>
+        <body>
+          <form action="/message" method="POST">
+            <input type="text" name="message" />
+            <button type="submit">Submit</button>
+          </form>
+        </body>
+      </html>
+    `);
+
+    return res.end();
+  }
+
   res.setHeader('Content-Type', 'text/html');
   res.write(`
     <html>
