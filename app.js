900a7c72eec4c8dc6387b5b872cf1f64e166b982
Jason Piros <jasonpiros@gmail.com>
2020-07-01 20:39:54 -0700

    redirect a request

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(( req, res ) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === '/') {
     res.write(`
@@ -18,6 +20,14 @@ const server = http.createServer(( req, res ) => {
       </html>
     `);
 
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'IMS');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    
     return res.end();
   }
 
