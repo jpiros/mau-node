29f157b333b01274bd7a8d8703d3cea23499c474
Jason Piros <jasonpiros@gmail.com>
2020-07-01 20:29:07 -0700

    send a html response

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 const server = http.createServer(( req, res ) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`
+    <html>
+      <head>
+        <title>IMS</title>
+      </head>
+      <body>
+        <h1>The best things in life start with Mau.</h1>
+      </body>
+    </html>
+  `);
+  res.end();
 });
 
 server.listen(7505);
