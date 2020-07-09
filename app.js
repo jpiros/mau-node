c6cba1fcf8f5298147befca8f0d833b02425f78e
Jason Piros <jasonpiros@gmail.com>
2020-07-08 22:59:25 -0700

    use express router, handle 404s, filter routes

--- app.js
+++ app.js
@@ -3,31 +3,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use('/', (req, res, next) => {;
-  console.log('gmml');
-  next();
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {;
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title" />
-      <button type="submit">Add Product</button>
-    </form>
-  `);
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send(`<h1>The Best Things In Life Start With Mau</h1>`);
-});
+app.use((req, res, next) => {
+  res.status(404).send(`
+    <h1>Page Not Found (probably got forced to move to FL)</h1>
+  `)
+})
 
 app.listen(7505);
